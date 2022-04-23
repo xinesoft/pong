f3f20b22fe40498e18439cf3af16e2c819cc2154
Fabio Garcia Balbuena <fagarbal@gmail.com>
2022-04-23 21:54:50 +0200

    added sounds, improved bounce

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -9,19 +9,25 @@ var direction = Vector2.LEFT;
 
 onready var ScoreRight = get_parent().get_node("ScoreRight")
 onready var ScoreLeft = get_parent().get_node("ScoreLeft")
+onready var Paddle = get_parent().get_node("Paddle")
 
 func _ready():
 	initialPosition = position;
+
 
 func _physics_process(delta):
 	var collision = move_and_collide(direction * SPEED * delta)
 
 	if collision and (collision.collider.name == 'Paddle' or collision.collider.name == 'Enemy'):
-		var collision_pos = collision.collider.position - position
-		direction.y = collision_pos.normalized().y * -1;
+		var collision_pos = collision.collider.position.y - position.y
+		var normalized = range_lerp(collision_pos, 0, 21, 0, 1)
+		
+		direction.y = normalized * -1;
 		direction.x *= -1
+		$bounce_fx.play()
 
 	if position.y < 0 or position.y > screen_size.y:
+		$bounce_fx.play()
 		direction.y *= -1
 	
 	if position.x < 0:
@@ -29,10 +35,12 @@ func _physics_process(delta):
 		
 		position = initialPosition
 		direction.y = 0
+		$lose_fx.play()
 	
 	if position.x > screen_size.x:
 		ScoreLeft.text = str(int(ScoreLeft.text) + 1)
 		
 		position = initialPosition
 		direction.y = 0
+		$win_fx.play()
 
